d78d089f0ae06997f586b34605bea87eec2737c2
jackson1992121 <dantes0121@hotmail.com>
2020-11-05 12:05:03 +0800

    change requirements.txt

--- accounts/serializers/member.py
+++ accounts/serializers/member.py
@@ -12,23 +12,25 @@ from drf_extra_fields.fields import Base64ImageField
 from accounts.models import Member, Media
 from datetime import datetime
 
+
 def file_validator(file):
     max_file_size = 1024 * 1024 * 1  # 1MB
 
     if file.size > max_file_size:
         raise serializers.ValidationError(_('Max file size is {} and your file size is {}'.
-            format(max_file_size, file.size)))
+                                            format(max_file_size, file.size)))
+
 
 class InitialInfoRegisterSerializer(serializers.Serializer):
-    media = serializers.FileField( max_length = 1000000, allow_empty_file = False, use_url = False,
-        validators = [file_validator], write_only = True )
+    media = serializers.FileField(max_length=1000000, allow_empty_file=False, use_url=False,
+                                  validators=[file_validator], write_only=True)
     nickname = serializers.CharField()
     birthday = serializers.CharField()
 
     def update(self, instance, validated_data):
         # save media
         image = validated_data.pop('media')
-        media_image = Media.objects.create(uri = image)
+        media_image = Media.objects.create(uri=image)
 
         # save user
         instance.nickname = validated_data['nickname']
@@ -38,4 +40,3 @@ class InitialInfoRegisterSerializer(serializers.Serializer):
         instance.save()
 
         return instance
-
