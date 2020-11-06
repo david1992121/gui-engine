eb418f74a79e181cc50f377ea08f60b1415566d6
terminator <terminator940120@yahoo.co.jp>
2020-11-07 02:37:09 +0800

    fix some lint warnings

--- accounts/serializers/member.py
+++ accounts/serializers/member.py
@@ -2,18 +2,19 @@
 Serializers for Member
 """
 
+from datetime import datetime
+
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
 
-from rest_framework.pagination import PageNumberPagination
 from drf_extra_fields.fields import Base64ImageField
 from accounts.models import Media, Tweet, Member
 from accounts.serializers.auth import MediaImageSerializer
-from datetime import datetime
-from rest_framework.response import Response
 
 
 def file_validator(file):
@@ -44,6 +45,7 @@ class InitialInfoRegisterSerializer(serializers.Serializer):
 
         return instance
 
+
 class MainInfoSerializer(serializers.ModelSerializer):
     avatars = MediaImageSerializer(read_only=True, many=True)
 
@@ -57,23 +59,32 @@ class MainInfoSerializer(serializers.ModelSerializer):
         )
         model = Member
 
+
 class TweetSerializer(serializers.ModelSerializer):
     medias = serializers.ListField(
-        child = serializers.FileField( max_length = 100000,
-            allow_empty_file=False, use_url=False, validators=[file_validator] )
-        , write_only = True
+        child=serializers.FileField(
+            max_length=100000,
+            allow_empty_file=False,
+            use_url=False,
+            validators=[file_validator]
+        ),
+        write_only=True,
+        required=False
     )
     likers = serializers.SerializerMethodField()
-    user = MainInfoSerializer(read_only = True)
-    images = MediaImageSerializer(read_only = True, many = True)
-    user_id = serializers.IntegerField(write_only = True)
+    user = MainInfoSerializer(read_only=True)
+    images = MediaImageSerializer(read_only=True, many=True)
+    user_id = serializers.IntegerField(write_only=True)
+
     class Meta:
-        fields = ("id", "content", "images", "user", "likers", "created_at", "medias", "user_id")
+        fields = ("id", "content", "images", "user",
+                  "likers", "created_at", "medias", "user_id")
         model = Tweet
 
     def get_likers(self, obj):
         likers_id = obj.tweet_likers.all().order_by('-created_at').values_list('liker')
-        like_users = MainInfoSerializer(Member.objects.filter(id__in = likers_id, is_registered = True), many = True)
+        like_users = MainInfoSerializer(Member.objects.filter(
+            id__in=likers_id, is_registered=True), many=True)
         return like_users.data
 
     def create(self, validated_data):
@@ -82,18 +93,19 @@ class TweetSerializer(serializers.ModelSerializer):
         if 'medias' in validated_data.keys():
             media_images = validated_data.pop('medias')
             for media in media_images:
-                new_media = Media.objects.create(uri = media)
+                new_media = Media.objects.create(uri=media)
                 media_ids.append(new_media.id)
-        
-        newTweet = Tweet(**validated_data)
-        newTweet.user = Member.objects.get(pk = user_id)
-        newTweet.save()
-        newTweet.images.set(media_ids)
 
-        return newTweet
+        new_tweet = Tweet(**validated_data)
+        new_tweet.user = Member.objects.get(pk=user_id)
+        new_tweet.save()
+        new_tweet.images.set(media_ids)
+
+        return new_tweet
+
 
 class TweetPagination(PageNumberPagination):
     page_size = 10
 
-    def get_paginated_response(self, data):        
+    def get_paginated_response(self, data):
         return Response(data)
