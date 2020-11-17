6f8c7b9ae6221096a3c5707672c585d587018b61
terminator <terminator940120@yahoo.co.jp>
2020-11-17 22:32:47 +0800

    add chat routing to wsgi

--- gui/wsgi.py
+++ gui/wsgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.wsgi import get_wsgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gui.settings')
 
 application = get_wsgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_wsgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
